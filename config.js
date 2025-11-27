7f9cf45edf1634d66626463e6ed80edcc76c0c60
okokokkokk <kashigamming2008@gmail.com>
2025-11-28 01:39:36 +0700

    xd

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hà Vy';
+const giftUrl = 'https://www.instagram.com/just.tmio/';
+const eventName = 'Happy Birthday';
+const titleCard = 'Zy Zỷ Zỳ Zy';
+const contentCard = 'Chúc em có một ngày sinh nhật thật vui vẻ, hạnh phúc, tràn đầy niềm vui bên người thân và gia đình nhaaa. Chúc em tuổi mới ngày càng mạnh khoẻ, học giỏi, đạt danh hiệu hsxs và có được người mình thương tuyệt vời (như anh🐧) nheeee!!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'zy4.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
